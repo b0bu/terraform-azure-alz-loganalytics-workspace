7e9159de74a8ae4ff78394b1ca5cac264f9e9ccb
Jason Leitch <jasonleitch0@gmail.com>
2022-12-14 09:51:41 +0000

    chore: add tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "id" {
+  value = azurerm_log_analytics_workspace.law.id
+}
+output "name" {
+  value = azurerm_log_analytics_workspace.law.name
+}
+output "location" {
+  value = azurerm_log_analytics_workspace.law.location
+}
